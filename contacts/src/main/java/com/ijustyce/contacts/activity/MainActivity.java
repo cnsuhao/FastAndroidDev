e41d1cc05de8d4cbd78d104ea195eac75163d89b
yangchun <ijustyce@163.com>
2016-04-18 09:36:19 +0800

    lastest

--- contacts/src/main/java/com/ijustyce/contacts/activity/MainActivity.java
+++ contacts/src/main/java/com/ijustyce/contacts/activity/MainActivity.java
@@ -1,15 +1,10 @@
 package com.ijustyce.contacts.activity;
 
-import com.alibaba.fastjson.JSON;
 import com.ijustyce.contacts.R;
-import com.ijustyce.contacts.constant.Constant;
 import com.ijustyce.contacts.fragment.FriendsFragment;
 import com.ijustyce.contacts.fragment.MeFragment;
 import com.ijustyce.contacts.fragment.MsgFragment;
-import com.ijustyce.contacts.model.test;
 import com.ijustyce.fastandroiddev.base.BaseTabActivity;
-import com.ijustyce.fastandroiddev.baseLib.utils.IJson;
-import com.ijustyce.fastandroiddev.baseLib.utils.ILog;
 
 public class MainActivity extends BaseTabActivity {
 
@@ -27,43 +22,5 @@ public class MainActivity extends BaseTabActivity {
         addTab(R.layout.tab_friends, R.id.radioButton);
         addTab(R.layout.tab_msg, R.id.radioButton);
         addTab(R.layout.tab_me, R.id.radioButton);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                test gson = null, fast = null;
-
-                long gsonStart = System.currentTimeMillis();
-                for(int i = 0; i < 1; i ++) {
-                    gson = IJson.fromJson(Constant.test, test.class);
-                }
-                ILog.i("===Gson===", "start " + (System.currentTimeMillis() - gsonStart));
-
-                long fastStart = System.currentTimeMillis();
-                for(int i = 0; i < 1; i ++) {
-                    fast = JSON.parseObject(Constant.test, test.class);
-                }
-                ILog.i("===fast===", "start " + (System.currentTimeMillis() - fastStart));
-
-                long gsonStart1 = System.currentTimeMillis();
-                for(int i = 0; i < 1; i ++) {
-                    IJson.toJson(gson, test.class);
-                }
-                ILog.i("===Gson===", "start " + (System.currentTimeMillis() - gsonStart1));
-
-                long fastStart1 = System.currentTimeMillis();
-                for(int i = 0; i < 1; i ++) {
-                    JSON.toJSONString(fast);
-                }
-                ILog.i("===fast===", "start " + (System.currentTimeMillis() - fastStart1));
-
-            }
-        }).start();
     }
 }
