83c7d17b2e9764004bf2852803e5da0b980e778f
ijustyce <ijustyce@163.com>
2016-04-19 23:50:22 +0800

    修复init没调用

--- fastandroiddev/src/main/java/com/ijustyce/fastandroiddev/base/BaseTabFragment.java
+++ fastandroiddev/src/main/java/com/ijustyce/fastandroiddev/base/BaseTabFragment.java
@@ -31,6 +31,7 @@ public abstract class BaseTabFragment extends BaseFragment {
         return R.layout.fastandroiddev_fragment_tab;
     }
 
+    @Override
     final void doInit(){
 
         initData();
