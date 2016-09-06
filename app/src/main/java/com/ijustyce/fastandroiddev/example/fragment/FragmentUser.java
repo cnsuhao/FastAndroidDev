6cc46020d71a86abc92053ffbe020f14bbdae04b
ijustyce <ijustyce@163.com>
2016-09-06 12:37:50 +0800

    用户头像

--- app/src/main/java/com/ijustyce/fastandroiddev/example/fragment/FragmentUser.java
+++ app/src/main/java/com/ijustyce/fastandroiddev/example/fragment/FragmentUser.java
@@ -3,6 +3,7 @@ package com.ijustyce.fastandroiddev.example.fragment;
 import com.ijustyce.fastandroiddev.base.BaseFragment;
 import com.ijustyce.fastandroiddev.example.R;
 import com.ijustyce.fastandroiddev.example.databinding.ContactFragmentView;
+import com.ijustyce.fastandroiddev.example.model.UserInfo;
 
 /**
  * Created by yc on 2016/9/5 0005.
@@ -12,6 +13,14 @@ public class FragmentUser extends BaseFragment<ContactFragmentView>{
 
     @Override
     public int getLayoutId() {
-        return R.layout.fragment_contact;
+        return R.layout.fragment_user;
+    }
+
+    @Override
+    public void afterCreate() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.nickName.set(getResString(R.string.user_default_nickName));
+        userInfo.phone.set(getResString(R.string.user_default_phone));
+        contentView.setUser(userInfo);
     }
 }
